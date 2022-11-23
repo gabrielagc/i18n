33b454a8d400c7912b2a30067cea330f640e1a07
Gabriela Garcia <g.garciac@uniandes.edu.co>
2022-11-22 23:06:18 -0500

    ejercicio localizacion

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,5 +1,24 @@
 import React, { useState } from "react";
 import Job from "./job";
+import {FormattedMessage} from 'react-intl';
+import "./joblist.css";
+
+var idioma = "";
+
+const languaje = navigator.language;
+
+  if (languaje.includes("es")){
+    
+      idioma = "espanol"
+    
+  }else if (languaje.includes("en")){
+    
+    idioma =  "english"
+    
+  }
+
+  console.log(languaje)
+
 
 const JobsList = () => {
   const [offers] = useState([
@@ -10,6 +29,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 1000
     },
     {
       id: "0002",
@@ -18,6 +38,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 1500
     },
     {
       id: "0003",
@@ -26,20 +47,74 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 1600
     },
   ]);
 
-  return (
+
+  if(idioma === "espanol"){
+    return (
+      <div>
+        <table className="table ">
+          <thead className="thead-dark es">
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">
+                  <FormattedMessage id="Nombre del cargo"/>
+              </th>
+              <th scope="col">
+                  <FormattedMessage id="Empresa"/>
+              </th>
+              <th scope="col">
+                  <FormattedMessage id="Salario"/>
+              </th>
+              <th scope="col">
+                  <FormattedMessage id="Ciudad"/>
+              </th>
+              <th scope="col">
+                  <FormattedMessage id="Fecha de publicación"/>
+              </th>
+              <th scope="col">
+                  <FormattedMessage id="No. de visitas"/>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {console.log("Offers", offers)}
+            {offers.map((e, i) => (
+              <Job key={i} offer={e} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+
+  else if(idioma === "english"){ return (
     <div>
-      <table className="table">
+      <table className="table en"  >
         <thead className="thead-dark">
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+            <FormattedMessage id="Position"/>
+            </th>
+            <th scope="col">
+            <FormattedMessage id="Company"/>
+            </th>
+
+            <th scope="col">
+            <FormattedMessage id="Salary"/>
+            </th>
+            <th scope="col">
+            <FormattedMessage id="City"/>
+            </th>
+            <th scope="col">
+            <FormattedMessage id="Publication date"/>
+            </th>
+            <th scope="col">
+            <FormattedMessage id="Views"/>
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -51,6 +126,7 @@ const JobsList = () => {
       </table>
     </div>
   );
+  }
 };
 
 export default JobsList;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
 
 import JobsList from "./components/jobslist";
+import localeEsMessages from "./locales/en";
+import localeEnMessages from "./locales/en";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+ReactDOM.render(
+        <IntlProvider locale="es-ES" messages= {[localeEsMessages, localeEnMessages]}>
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+);
